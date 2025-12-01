e5d17b5a11384b6b2844c298e13c627bbd4516d1
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-12-01 15:24:30 +0100

    Ajout d'une classe Ingredient Oeuf et chocloat

--- patisserie.py
+++ patisserie.py
@@ -9,9 +9,8 @@ class Commis(threading.Thread, ABC):
         threading.Thread.__init__(self)
         self.nom = nom
 
-    def say(self, message):
+    def dire(self, message):
         print(f"[{self.nom}] {message}")
-
 
 class BatteurOeufs(Commis):
     def __init__(self, nom, nb_oeufs):
@@ -21,7 +20,7 @@ class BatteurOeufs(Commis):
     def run(self):
         nb_tours = self.nb_oeufs * 8
         for no_tour in range(1, nb_tours + 1):
-            self.say(f"Je bats les {self.nb_oeufs} oeufs, tour n°{no_tour}")
+            self.dire(f"Je bats les {self.nb_oeufs} oeufs, tour n°{no_tour}")
             time.sleep(0.5)
 
 
@@ -31,18 +30,37 @@ class FondeurChocolat(Commis):
         self.quantite = quantite  # en grammes
 
     def run(self):
-        self.say("Je mets de l'eau à chauffer dans une bouilloire")
+        self.dire("Je mets de l'eau à chauffer dans une bouilloire")
         time.sleep(8)
-        self.say("Je verse l'eau dans une casserole")
+        self.dire("Je verse l'eau dans une casserole")
         time.sleep(2)
-        self.say("J'y pose le bol rempli de chocolat")
+        self.dire("J'y pose le bol rempli de chocolat")
         time.sleep(1)
 
         nb_tours = math.ceil(self.quantite / 10)
 
         for no_tour in range(1, nb_tours + 1):
-            self.say(f"Je mélange {self.quantite} g de chocolat, tour n°{no_tour}")
+            self.dire(f"Je mélange {self.quantite} g de chocolat, tour n°{no_tour}")
             time.sleep(1)
+
+class Ingredient(threading.Thread, ABC):
+    def __init__(self, nom, quantite, unite):
+        threading.Thread.__init__(self)
+        self.nom = nom
+        self.quantite = quantite
+        self.unite = unite
+
+class Oeuf(Ingredient):
+    def __init__(self, nom, quantite, unite):
+        super().__init__(nom, quantite, unite)
+
+class Chocolat(Ingredient):
+    def __init__(self, nom, quantite, unite):
+        super().__init__(nom, quantite, unite)
+
+
+
+
 
 
 if __name__ == "__main__":
