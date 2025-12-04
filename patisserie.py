fc681b6d020e57b31e2a0ddf8485aa0644e04da4
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-12-04 15:15:39 +0100

    Ajout de la classe Appareill

--- patisserie.py
+++ patisserie.py
@@ -13,6 +13,7 @@ class Commis(threading.Thread, ABC):
     def run(self):
         pass
 
+
 class Ingredient(threading.Thread, ABC):
     def __init__(self, nom, quantite, unite):
         threading.Thread.__init__(self)
@@ -20,10 +21,29 @@ class Ingredient(threading.Thread, ABC):
         self.quantite = quantite
         self.unite = unite
 
+
+class Appareil:
+    """
+    Un appareil représente un mélange homogène d'ingrédients
+    avant cuisson ou turbinage.
+    """
+
+    def __init__(self, nom):
+        self.nom = nom
+        self.ingredients = []
+
+    def ajouter_ingredient(self, ingredient):
+        self.ingredients.append(ingredient)
+
+    def get_ingredients(self):
+        return self.ingredients[:]
+
+
 class BatteurOeufs(Commis):
     def __init__(self, nom, nb_oeufs):
         super().__init__(nom)
         self.nb_oeufs = nb_oeufs
+        self.appareil = appareil
 
     def run(self):
         nb_tours = self.nb_oeufs * 8
@@ -33,17 +53,18 @@ class BatteurOeufs(Commis):
 
 
 class FondeurChocolat(Commis):
-    def __init__(self, nom, quantite):
-        super().__init__(nom)
-        self.quantite = quantite  # en grammes
-
     class Oeuf(Ingredient):
-        def __init__(self, nom, quantite, unite):
-            super().__init__(nom, quantite, unite)
+        def __init__(self, quantite):
+            super().__init__("Œuf", quantite, "pièces")
 
     class Chocolat(Ingredient):
-        def __init__(self, nom, quantite, unite):
-            super().__init__(nom, quantite, unite)
+        def __init__(self, quantite):
+            super().__init__("Chocolat", quantite, "g")
+
+    def __init__(self, nom, quantite, appareil=None):
+        super().__init__(nom)
+        self.quantite = quantite
+        self.appareil = appareil
 
     def run(self):
         print(f"[{self.nom}] Je mets de l'eau à chauffer dans une bouilloire")
@@ -54,15 +75,31 @@ class FondeurChocolat(Commis):
         time.sleep(1)
 
         nb_tours = math.ceil(self.quantite / 10)
+
         for no_tour in range(1, nb_tours + 1):
             print(f"[{self.nom}] Je mélange {self.quantite} g de chocolat, tour n°{no_tour}")
             time.sleep(1)
 
 
+class Oeuf(Ingredient):
+    def __init__(self, nom, quantite, unite):
+        super().__init__(nom, quantite, unite)
+
+
+class Chocolat(Ingredient):
+    def __init__(self, nom, quantite, unite):
+        super().__init__(nom, quantite, unite)
+
+
 if __name__ == "__main__":
+    # Création d'un appareil
+    appareil = Appareil("Appareil chocolat-œufs")
+
+    # Commis
     batteur = BatteurOeufs("Jean", 6)
     fondeur = FondeurChocolat("Marie", 200)
 
+    # Lancement
     batteur.start()
     fondeur.start()
 
@@ -70,3 +107,10 @@ if __name__ == "__main__":
     fondeur.join()
 
     print("\nJe peux à présent incorporer le chocolat aux œufs")
+
+    #oeuf = Oeuf("Oeufs fermiers", 6, "pièces")
+    #chocolat = Chocolat("Chocolat noir", 200, "g")
+    #appareil.ajouter_ingredient(oeuf)
+    #appareil.ajouter_ingredient(chocolat)
+    #for ingredient in appareil.get_ingredients():
+    #   print(f"- {ingredient.quantite} {ingredient.unite} de {ingredient.nom}")
