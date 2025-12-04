815f0fca6915b3ceed3b6027687f278665f27174
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-12-04 14:16:10 +0100

    recommece a zero

--- patisserie.py
+++ patisserie.py
@@ -1,121 +1,46 @@
 import threading
 import time
 import math
-from abc import ABC
 
 
-class Commis(threading.Thread, ABC):
-    def __init__(self, nom, recipient):
+class BatteurOeufs(threading.Thread):
+    def __init__(self, nb_oeufs):
         threading.Thread.__init__(self)
-        self.nom = nom
-        self.recipient = recipient
-
-    def dire(self, message):
-        print(f"[{self.nom} - {self.recipient.nom}] {message}")
-
-
-class BatteurOeufs(Commis):
-    def __init__(self, nom, nb_oeufs, recipient):
-        super().__init__(nom, recipient)
         self.nb_oeufs = nb_oeufs
 
     def run(self):
+        # on suppose qu'il faut 8 tours de batteur par œuf présent dans le bol
         nb_tours = self.nb_oeufs * 8
         for no_tour in range(1, nb_tours + 1):
-            self.dire(f"Je bats les {self.nb_oeufs} {self.recipient.contenu.nom}, tour n°{no_tour}")
-            time.sleep(0.5)
+            print(f"\tJe bats les {self.nb_oeufs} oeufs, tour n°{no_tour}")
+            time.sleep(0.5)  # temps supposé d'un tour de batteur
 
 
-class FondeurChocolat(Commis):
-    def __init__(self, nom, quantite, recipient):
-        super().__init__(nom, recipient)
+class FondeurChocolat(threading.Thread):
+    def __init__(self, quantite):
+        threading.Thread.__init__(self)
         self.quantite = quantite  # en grammes
 
     def run(self):
-        self.dire("Je mets de l'eau à chauffer dans une bouilloire")
+        print("Je mets de l'eau à chauffer dans une bouilloire")
         time.sleep(8)
-        self.dire("Je verse l'eau dans une casserole")
+        print("Je verse l'eau dans une casserole")
         time.sleep(2)
-        self.dire(f"J'y pose le {self.recipient.nom} rempli de {self.recipient.contenu.nom}")
+        print("J'y pose le bol rempli de chocolat")
         time.sleep(1)
-
+        # on suppose qu'il faut 1 tour de spatule par 10 g. de chocolat
+        # présent dans le bol pour faire fondre le chocolat
         nb_tours = math.ceil(self.quantite / 10)
-
         for no_tour in range(1, nb_tours + 1):
-            self.dire(f"Je mélange {self.quantite} {self.recipient.contenu.unite} de {self.recipient.contenu.nom} "
-                      f"tour n°{no_tour}")
-            time.sleep(1)
-
-
-class Ingredient(threading.Thread, ABC):
-    def __init__(self, nom, quantite, unite):
-        threading.Thread.__init__(self)
-        self.nom = nom
-        self.quantite = quantite
-        self.unite = unite
-
-
-class Oeuf(Ingredient):
-    def __init__(self, nom, quantite, unite):
-        super().__init__(nom, quantite, unite)
-
-
-class Chocolat(Ingredient):
-    def __init__(self, nom, quantite, unite):
-        super().__init__(nom, quantite, unite)
-
-
-class Appareil:
-    def __init__(self, nom):
-        self.nom = nom
-        self.ingredients = []
-
-    def ajouter(self, ingredient):
-        """Ajoute un ingrédient (objet Ingredient)."""
-        self.ingredients.append(ingredient)
-
-    def melanger(self):
-        """Simule un mélange homogène."""
-        print(f"\nJe mélange  '{self.nom}' pour obtenir une préparation homogène…")
-        time.sleep(2)
-        print(f"L'appareil '{self.nom}' est prêt.\n")
-
-
-class Recipient:
-    def __init__(self, nom, contenu):
-        self.nom = nom
-        self.contenu = contenu
-
-    def ajouter(self, contenu):
-        self.contenu = contenu
+            print(f"Je mélange {self.quantite} de chocolat à fondre, tour n°{no_tour}")
+            time.sleep(1)  # temps supposé d'un tour de spatule
 
 
 if __name__ == "__main__":
-
-    oeufs = Oeuf("oeuf fermier", 6, "unités")
-    chocolat_patissier = Chocolat("chocolat patissier", 200, "g")
-    chocolat_noir = Chocolat("chocolat noir", 100, "g")
-
-    recipient_batteur = Recipient("Casserole", oeufs)
-    recipient_fondeurA = Recipient("Bol en Inox A", chocolat_patissier)
-    recipient_fondeurB = Recipient("Bol en Inox B", chocolat_noir)
-
-    batteur = BatteurOeufs("Pierre", 6, recipient_batteur)
-    fondeurA = FondeurChocolat("Marie", 200, recipient_fondeurA)
-    fondeurB = FondeurChocolat("Lucas", 200, recipient_fondeurB)
-
+    batteur = BatteurOeufs(6)
+    fondeur = FondeurChocolat(200)
     batteur.start()
-    fondeurA.start()
-    fondeurB.start()
-
+    fondeur.start()
     batteur.join()
-    fondeurA.join()
-    fondeurB.join()
-
+    fondeur.join()
     print("\nJe peux à présent incorporer le chocolat aux oeufs")
-
-    appareil = Appareil("Base Gateau au chocolat noir et patissier")
-    appareil.ajouter(oeufs)
-    appareil.ajouter(chocolat_patissier)
-    appareil.ajouter(chocolat_noir)
-    appareil.melanger()
